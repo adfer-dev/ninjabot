d482e6093967d0810081e6fa86a751c4c7c76b8c
adferdev <adferdev@gmail.com>
2023-04-18 23:21:05 +0200

    style: change bot messages and delete welcome command

--- controllers/commands.js
+++ controllers/commands.js
@@ -1,36 +1,21 @@
 import { SlashCommandBuilder, ChannelType, PermissionsBitField } from 'discord.js'
 
+const TAGGED_USER_ID_PATTERN = /<@(\d{19})>/g
+
 const commands = [
-  // welcome channel command.
-  {
-    data: new SlashCommandBuilder()
-      .setName('welcome')
-      .setDescription('creates a welcome channel on the server.'),
-    async execute (interaction) {
-      if (!interaction.guild.channels.cache.find(channel => channel.name === 'welcome')) {
-        await interaction.guild.channels.create({
-          name: 'welcome',
-          type: ChannelType.GuildText
-        })
-        await ephemeralInteractionReply(interaction, 'welcome channel has been created successfully.')
-      } else {
-        await ephemeralInteractionReply(interaction, 'welcome channel has already been created.')
-      }
-    }
-  },
   // private room command.
   {
     data: new SlashCommandBuilder()
       .setName('privateroom')
-      .setDescription('creates a temporal channel that only the tagged users can see.')
+      .setDescription('Creates a private channel that only this user and the tagged users can see')
       .addStringOption(nameOption =>
         nameOption.setName('name')
-          .setDescription('the name of the room')
+          .setDescription('Name of the room')
           .setRequired(true)
       )
       .addIntegerOption(typeOption =>
         typeOption.setName('type')
-          .setDescription('the type of the channel. only voice or text are valid inputs.')
+          .setDescription('Type of the channel')
           .setRequired(true)
           .addChoices(
             { name: 'voice', value: ChannelType.GuildVoice },
@@ -39,73 +24,76 @@ const commands = [
       )
       .addStringOption(usersOption =>
         usersOption.setName('users')
-          .setDescription('the users to be added to the private room.')
-          .setRequired(true)
+          .setDescription('Additional users to be added to the private room')
+          .setRequired(false)
       ),
     async execute (interaction) {
-      const userIds = interaction.options.getString('users').replaceAll(/[^\d ^\s]/g, '').split(' ') // get users´s ids passed by the user
-      const channelName = interaction.options.getString('name')// the user´s selected channel room
+      const newChannel = await interaction.guild.channels.create({
+        name: 'private_' + interaction.options.getString('name'),
+        type: interaction.options.getInteger('type')
+      })
 
-      // check that all tagged users exist.
-      if (!userIds.includes('')) {
-        const newChannel = await interaction.guild.channels.create({
-          name: 'private_' + channelName,
-          type: interaction.options.getInteger('type')
-        })
+      // grant access to invoker user
+      newChannel.permissionOverwrites.create(interaction.member.id, {
+        ViewChannel: true
+      })
+      // also grant access to bot user
+      newChannel.permissionOverwrites.create(interaction.client.user.id, {
+        ViewChannel: true
+      })
+      // deny access to the rest of users
+      newChannel.permissionOverwrites.create(interaction.guild.roles.cache.find(deniedRoles => deniedRoles.name === '@everyone'), {
+        ViewChannel: false,
+        CreateInstantInvite: false
+      })
 
-        // set channel´s permissions only for the chosen users.
-        for (const id of userIds) {
-          newChannel.permissionOverwrites.create(id, {
-            ViewChannel: true
-          })
-        }
-        // also grant access to bot user
-        newChannel.permissionOverwrites.create(interaction.guild.roles.cache.find(botRole => botRole.name === 'adferbot').id, {
-          ViewChannel: true
-        })
-        // deny access to the rest of users
-        newChannel.permissionOverwrites.create(interaction.guild.roles.cache.find(deniedRoles => deniedRoles.name === '@everyone'), {
-          ViewChannel: false
-        })
-
-        await ephemeralInteractionReply(interaction, 'Private channel has been created')
-
-        // set an interval to check every 2 hours if the channel is empty. If it is, delete it.
-        const checkUnusedInterval = setInterval(() => {
-          if (newChannel.members.size === 0) {
-            // first check that the channel has not been manually deleted
-            if (interaction.guild.channels.cache.has(newChannel.id)) {
-              newChannel.delete('Temporal channel expired - Delete channel')
-            }
-            clearInterval(checkUnusedInterval)
+      // check if there are additional users and grant them permissions if they are valid
+      if (interaction.options.getString('users')) {
+        const validTaggedUsers = interaction.options.getString('users').match(TAGGED_USER_ID_PATTERN) // get users´s ids passed by the user that match the format(<@idNumber>)
+        if (validTaggedUsers) {
+          const userIds = validTaggedUsers.map(inputUserId => inputUserId.replaceAll(/[^0-9]/g, '')) // get just the id numbers
+          for (const id of userIds) {
+            newChannel.permissionOverwrites.create(id, {
+              ViewChannel: true
+            })
           }
-        }, 7200000)
-      } else {
-        await ephemeralInteractionReply(interaction, 'Input error: This channel\'s name already exists')
+        }
       }
+      await ephemeralInteractionReply(interaction, 'Private channel has been created. Now you can use **/getcodes** to get the codes of each private channel. Any user can join your channel using **/joinchannel** and pasting in the code.')
+
+      // set an interval to check every 2 hours if the channel is empty. If it is, delete it.
+      const checkUnusedInterval = setInterval(() => {
+        if (newChannel.members.size === 0) {
+          // first check that the channel has not been manually deleted
+          if (interaction.guild.channels.cache.has(newChannel.id)) {
+            newChannel.delete('Temporal channel expired - Delete channel')
+          }
+          clearInterval(checkUnusedInterval)
+        }
+      }, 7200000)
     }
   },
 
   {
     data: new SlashCommandBuilder()
       .setName('getcodes')
-      .setDescription('gets the codes of the private channels that this user is in.'),
+      .setDescription('Gets the codes of the private channels that this user is in'),
     async execute (interaction) {
       const channels = interaction.guild.channels.cache.filter(channel => (channel.permissionsFor(interaction.member).has(PermissionsBitField.Flags.ViewChannel) && channel.name.startsWith('private_')))
       if (channels.size > 0) {
-        let messageContent = 'Codes of private channels'
+        let messageContent = '__**Codes of private channels**__'
         channels.forEach(channel => {
-          messageContent += '\n' + channel.name + ': ' + channel.id
+          messageContent += '\n' + '**' + channel.name + '**: ' + channel.id
         })
         await ephemeralInteractionReply(interaction, messageContent)
-      } else { await ephemeralInteractionReply(interaction, 'You are not in any private channel.') }
+      } else { await ephemeralInteractionReply(interaction, 'You are not in any private channel. Please, create or join one first. ') }
     }
   },
 
   {
     data: new SlashCommandBuilder()
       .setName('joinchannel')
-      .setDescription('adds a user to a private channel')
+      .setDescription('Add this user to the channel that relates to the input code')
       .addStringOption(codeOption =>
         codeOption.setName('code')
           .setDescription('the code of the room to be joined')
@@ -114,10 +102,9 @@ const commands = [
     async execute (interaction) {
       const selectedChannel = interaction.guild.channels.cache.find(channel => channel.id === interaction.options.getString('code'))
       if (selectedChannel) {
-        console.log(selectedChannel.name)
         selectedChannel.permissionOverwrites.create(interaction.member.id, { ViewChannel: true })
-        await ephemeralInteractionReply(interaction, 'joining room...')
-      } else { await ephemeralInteractionReply(interaction, 'This channel does not exist.') }
+        await ephemeralInteractionReply(interaction, 'Joining ' + selectedChannel.name + '...')
+      } else { await ephemeralInteractionReply(interaction, 'Code incorrect. This channel does not exist. ') }
     }
   }
 ]
@@ -136,7 +123,7 @@ export async function initCommands (client) {
           await command.execute(interaction)
         } catch (error) {
           console.error(error)
-          await ephemeralInteractionReply(interaction, 'There was an error while executing this command!')
+          await ephemeralInteractionReply(interaction, 'Server error: There was an error while executing this command!')
         }
       }
     })
